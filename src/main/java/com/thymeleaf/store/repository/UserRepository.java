c1da9c1744d44c265d58e0eb4db66880b82bc306
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2023-01-24 20:17:08 +0200

    Created added findByUser and findByEmail methods

--- src/main/java/com/thymeleaf/store/repository/UserRepository.java
+++ src/main/java/com/thymeleaf/store/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.thymeleaf.store.entity.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<MyUser, Long> {
+    MyUser findByUsernameIgnoreCase(String username);
+
+    MyUser findByEmail(String username);
 }
