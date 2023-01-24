aac6221ed069570796abd5683facc7c3d684db09
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2023-01-24 20:57:48 +0200

    Created constants for Roles

---
+++ src/main/java/com/thymeleaf/store/util/Constants.java
@@ -0,0 +1,9 @@
+package com.thymeleaf.store.util;
+
+public final class Constants {
+
+    public static final String REDIRECT_TO_INDEX = "redirect:/index";
+
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_USER = "ROLE_USER";
+}
