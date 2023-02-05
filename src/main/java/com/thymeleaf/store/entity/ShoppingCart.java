f543d6888e5871f68999281f7f23769a770f31a1
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2023-02-05 17:36:25 +0200

    created shopping cart entity and repository

---
+++ src/main/java/com/thymeleaf/store/entity/ShoppingCart.java
@@ -0,0 +1,36 @@
+package com.thymeleaf.store.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShoppingCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Product> products = new ArrayList<>();
+
+    @OneToOne(mappedBy = "shoppingCart")
+    private MyUser user;
+
+    public void addProductToShoppingCart(Product p) {
+        this.products.add(p);
+    }
+
+    public void removeProductFromShoppingCart(Product product) {
+        this.products.remove(product);
+    }
+}
